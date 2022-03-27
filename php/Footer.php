c41ecc7f9f559d630cf93c4e8a9d902d97050375
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-27 11:04:46 +0700

    this is main project

---
+++ php/Footer.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="./model/css/footer.css">
+<div class="container-fluid" id="footer">
+    <div class="container">
+
+        <div id="title">
+            <h1>ĐĂNG KÍ ĐỂ NHẬN ƯU ĐÃI VÀ TƯ VẤN MIỄN PHÍ</h1>
+        </div>
+        <div class="cont">
+            <div class="left-cont">
+                <div class="display-img">
+                    <img src="./model/img/image.png" alt="aaaaaaaaaaaaa">
+                </div>
+                <ul>
+                    <li>Địa chỉ: Chung cư Citizen đường 9A KDC Trung Sơn, xã Bình Hưng, huyện Bình Chánh, TPHCM</li>
+                    <br>
+                    <li>Hotline: 093 543 6677</li>
+                    <br>
+                    <li>Email: bdsphankimngoc@gmail.com</li>
+
+                </ul>
+            </div>
+            <div class="right-cont">
+                <br>
+                <h2>Nhập thông tin liên hệ </h2>
+                <br>
+                <p>Chúng tôi đã giúp hàng nghìn khách hàng tìm được dự án ưng ý. Với quỹ hàng đa dạng nhất thị trường,
+                    chúng tôi tự tin sẽ giúp bạn thực hiện giấc mơ sơ hữu BĐS và tối ưu hiệu quả đầu tư.</p>
+                <br>
+                <input type="text" placeholder="Tên của bạn"> <br> <br>
+                <input type="text" placeholder="Địa chỉ email của bạn"> <br><br>
+                <input type="text" placeholder="Số điện thoại của bạn"> <br> <br>
+                <textarea rows="4" cols="" placeholder="Nội dung yêu cầu dự án..."></textarea>
+                <br> <br>
+                <input type="submit" value="Liên hệ báo giá">
+                <br> <br>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ php/Header.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="./model/css/header.css">
+<div class="header">
+    <div class="infor">
+        <div class="hotline">
+            <p><i class="fa fa-volume-control-phone"></i> Hotline: 0965 360 174</p>
+        </div>
+        <div class="email">
+            <p><i class="fa fa-envelope-o"></i> Email: manhthinhcuong@gmail.com</p>
+        </div>
+    </div>
+    <div class="menu">
+        <div class="logo-image">
+            <img src="./model/img/image.png" alt="">
+        </div>
+        <nav class="style-4">
+            <ul class="menu-4">
+                <li class="current"><a href="#" data-hover="Trang chủ">Trang chủ</a></li>
+                <li><a href="#" data-hover="Giới thiệu">Giới thiệu</a></li>
+                <li><a href="#" data-hover="Nhà">Nhà</a></li>
+                <li><a href="#" data-hover="Liên hệ">Liên hệ</a></li>
+            </ul>
+        </nav>
+        <div class="box">
+            <form action="" class="container-4">
+                <input type="search" id="search" placeholder="Search..." />
+                <button class="icon"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="virtual-class"></div>
